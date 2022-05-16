419418da8261d308ff04749fad7fa08304c27c23
Mackery <70108834+homelikezero@users.noreply.github.com>
2022-05-16 13:32:53 -0500

    remove

--- source/GJKeys.hx
+++
@@ -1,7 +0,0 @@
-package;
-
-class GJKeys
-{
-    public static var id:Int = 	716578; // Put your game's ID here
-    public static var key:String = "34cc9d8890051251827256bab36fcf2b"; // Put your game's private API key here, you can find it in your game's settings in GameJolt under the API tab!
-}
